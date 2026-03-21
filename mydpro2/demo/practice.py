dff9acfa456609a6d445a4d313b3a39fdaa20475
Harshitha kmurthy <kharshitha724@gmail.com>
2026-03-21 16:36:09 +0530

    oops concept

--- mydpro2/demo/practice.py
+++ mydpro2/demo/practice.py
@@ -24,7 +24,7 @@ for i in range(len(arr1)-1):
 print(res)     
 
 '''
-
+'''
 n=int(input("enter a num"))
 arr1=[2,3,6,1,4,8]
 res=[]
@@ -39,6 +39,8 @@ while i<len(arr1)-1:
 
 print(res)     
 
+'''
+
 ''' Instructions
     Inside the editor, complete the following steps:
     Create a class called Person
@@ -46,7 +48,7 @@ print(res)
     Add a method called greet that prints "Hello, my name is " followed by the name
     Create an object p1 of the class with name "John" and age 36
     Call the greet method on p1     '''
-
+'''
 
 class Person:
   def __init__(self ,name,age):
@@ -55,8 +57,38 @@ class Person:
   def greet(self):
       print(f"hello my name is {self.name}")
 p1=Person("john",36)
+p2=Person("riya",20)
 p1.greet()
+p2.greet()
+'''
 
+'''
+#without __init__()
+class Person1:
+    def greet1(abc):
+       print(f"hello my name is {abc.name1}")
+
+p2=Person1()
+p2.name1="xyz"
+p2.age=47
+p2.greet1()   '''
+
+
+# calling methods with self
+
+class Party:
+      def __init__(self,name):
+           self.name=name
+
+      def rec(self):
+           return  "hello " + self.name
+
+      def greet(self):
+            message=self.rec()
+            print(message + "  welcome to party")
+
+p1=Party("friend")
+p1.greet()                
 
 
 
