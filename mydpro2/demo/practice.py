f286f17e4b42f57c8c2b5fc19dd4984a86ecf7f4
Harshitha kmurthy <kharshitha724@gmail.com>
2026-03-23 16:15:21 +0530

    inheretance

--- mydpro2/demo/practice.py
+++ mydpro2/demo/practice.py
@@ -73,7 +73,7 @@ p2.name1="xyz"
 p2.age=47
 p2.greet1()   '''
 
-
+'''
 # calling methods with self
 
 class Party:
@@ -91,9 +91,112 @@ p1=Party("friend")
 p1.greet()                
 
 
+menu = {'Pizza': 89.0, 'Pasta': 60.0, 'Burger': 79.0, 'Salad': 30.0}
+total = 0.0
+print("Menu:", menu)
+
+while True:
+        item = input("Order item (or 'exit'): ").title()
+        
+        if item == 'Exit':
+            break
+
+        if item in menu:
+            try:
+                qty = int(input(f"Quantity for {item}: "))
+                if qty < 0:
+                    print("Quantity cannot be negative.")
+                    continue
+                total += menu[item] * qty
+            except ValueError:
+                print("Please enter a valid number.")
+        else:
+            print("Item not found.")
+
+    
+print(f"Total: ₹{total:.2f}")
 
 
 
+# super() = Function used in a child class to call methods from a parent 
+# to extend the functionality of inherited methods
 
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+    def describe(self):
+        print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+    def describe(self):
+        print(f"It is a circle with an area of {3.14 * self.radius * self.radius}cm^2")
+        super().describe()
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+    def describe(self):
+        print(f"It is a square with an area of {self.width * self.width}cm^2")
+        super().describe()
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+    def describe(self):
+        print(f"It is a triangle with an area of {self.width * self.height / 2}cm^2")
+        super().describe()
+
+circle = Circle(color="red", is_filled=True, radius=5)
+square = Square(color="blue", is_filled=False, width=6)
+triangle = Triangle(color="yellow", is_filled=True, width=7, height=8)
+
+circle.describe()
+#square.describe()
+#triangle.describe()
+
+'''
+
+
+class menu:
+    def __init__(self):
+        self.menu={'pizza':100,'burger':70,'pepsi':40,'combo':150}
+
+    def menudisp(self):
+        print("menu:",self.menu)
+
+class order(menu):
+    def __init__(self):
+        super().__init__()
+        self.total=0
+
+    def caltotal(self):
+      while True:
+           item = input("Order item (or 'exit'): ")
+           if item == 'exit': break
+           if item in self.menu:
+                 qty = int(input(f"Quantity for {item}: "))
+                 self.total += self.menu[item] * qty
+           else:
+                print("Item not found.")      
+
+    def totaldis(self):
+        print(f"total amount:{self.total}")
+
+o1=order()
+o1.menudisp()
+o1.caltotal()
+o1.totaldis()    
+                       
 
   
